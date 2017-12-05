6ede7784118e5cb596aed4160f5299fcb3db5b30
xiaxueliang <bjxiaxueliang@corp.netease.com>
2017-12-05 15:53:40 +0800

    贝塞尔曲线水波纹

---
+++ README.md
@@ -0,0 +1,15 @@
+
+### 贝赛尔曲线 的应用
+
+### 贝赛尔曲线不错的文章
+
+http://www.html-js.com/article/1628
+
+### 案例学习自：
+
+http://blog.csdn.net/aigestudio/article/details/41960507
+
+
+
+### 效果图
+![Android_Widget_WaveView](http://img.blog.csdn.net/20141216160349297)
---
+++ app/src/main/java/com/xiaxl/wave/MainApplation.java
@@ -0,0 +1,14 @@
+package com.xiaxl.wave;
+
+import android.app.Activity;
+import android.app.Application;
+
+public class MainApplation extends Application {
+
+    public static Activity textActivity;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
